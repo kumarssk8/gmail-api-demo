afe661141b36e0c41049ff4b61e58dc473f8e04a
kumarssk8 <kumarssk8@gmail.com>
2025-02-04 23:05:20 +0530

    config update

--- db_config.py
+++ db_config.py
@@ -4,6 +4,3 @@ os.environ["hf_mysql_host"]="127.0.0.1"
 os.environ["hf_mysql_user"] = "root"
 os.environ["hf_mysql_password"]="password"  
 os.environ["hf_db_name"] = "gmail_api_demo"
-
-batch_size = 10
-list_iteration = 10
